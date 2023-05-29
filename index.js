2a48b7ceb8c5f4521d1cdd626d679d4d623b75db
Doi <67437989+bryandoi@users.noreply.github.com>
2023-05-29 23:51:06 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
         var buttonInnerHTML = this.innerHTML;
 
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
 
        }); 
 }
@@ -16,60 +17,69 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
 
 // Detecting Keyboard Press
 
-    document.addEventListener("keydown", function(keyboard) {        
+    document.addEventListener("keydown", function(event) {        
 
-       makeSound(keyboard.key);
+        makeSound(event.key);
+        buttonAnimation(event.key);
 
     });
 
     function makeSound(key) {  
 
         switch (key) {
-        case "1":
+        case "q":
             var crash = new Audio("sounds/antimage.mp3");
             crash.play();
         
             break;
 
-        case "2":
+        case "w":
             var kickBass = new Audio("sounds/axe.mp3");
             kickBass.play();
             break;
 
-        case "3":
+        case "e":
             var snare = new Audio("sounds/hoodwink.mp3");
             snare.play();
             break;
 
-        case "4":
+        case "a":
             var tom1 = new Audio("sounds/invoker.mp3");
             tom1.play();
             break;
 
-        case "5":
+        case "s":
             var tom2 = new Audio("sounds/pudge.mp3");
             tom2.play();
             break;
 
-        case "6":
+        case "d":
             var tom3 = new Audio("sounds/rubick.mp3");
             tom3.play();
             break;
        
-        case "7":
+        case "z":
             var tom4 = new Audio("sounds/sniper.mp3");
             tom4.play();
             break;
 
         default:
             break;
-       };
+       }
     
+    }
+
+
+function buttonAnimation(event)  {
+
+    var activeButton = document.querySelector("." + event);
     
-    };
+    activeButton.classList.add("pressed");
 
-
-
+    setTimeout(function() {
+        activeButton.classList.remove("pressed"); 
+    }, 2500);
+}
 
 
 //   var audio = new Audio("sounds/snare.mp3");
